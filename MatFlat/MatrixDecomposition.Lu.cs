c0bf6f61a96279e7b81792a48b63470b7737e95e
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-03-14 23:20:31 +0900

    Try loop unrolling.

--- MatFlat/MatrixDecomposition.Lu.cs
+++ MatFlat/MatrixDecomposition.Lu.cs
@@ -19,26 +19,19 @@ namespace MatFlat
                 fixed (double* luColj = buffer)
                 {
                     // Outer loop.
+                    var colj = a;
                     for (var j = 0; j < n; j++)
                     {
-                        var indexj = j * lda;
-                        var indexjj = indexj + j;
-
                         // Make a copy of the j-th column to localize references.
-                        new Span<double>(&a[indexj], m).CopyTo(new Span<double>(luColj, m));
+                        new Span<double>(colj, m).CopyTo(new Span<double>(luColj, m));
 
                         // Apply previous transformations.
                         for (var i = 0; i < m; i++)
                         {
                             // Most of the time is spent in the following dot product.
                             var kmax = Math.Min(i, j);
-                            var s = 0.0;
-                            for (var k = 0; k < kmax; k++)
-                            {
-                                s += a[(k * lda) + i] * luColj[k];
-                            }
-
-                            a[indexj + i] = luColj[i] -= s;
+                            var s = Dot(kmax, a + i, lda, luColj, 1);
+                            colj[i] = luColj[i] -= s;
                         }
 
                         // Find pivot and exchange if necessary.
@@ -65,13 +58,15 @@ namespace MatFlat
                         }
 
                         // Compute multipliers.
-                        if (j < m && a[indexjj] != 0.0)
+                        if (j < m && colj[j] != 0.0)
                         {
                             for (var i = j + 1; i < m; i++)
                             {
-                                a[indexj + i] /= a[indexjj];
+                                colj[i] /= colj[j];
                             }
                         }
+
+                        colj += lda;
                     }
                 }
             }
@@ -79,6 +74,37 @@ namespace MatFlat
             {
                 ArrayPool<double>.Shared.Return(buffer);
             }
+        }
+
+        private static unsafe double Dot(int n, double* x, int incx, double* y, int incy)
+        {
+            var rem = n;
+            var sum1 = 0.0;
+            var sum2 = 0.0;
+            var ix1 = 0;
+            var iy1 = 0;
+            var ix2 = incx;
+            var iy2 = incy;
+            var incx2 = 2 * incx;
+            var incy2 = 2 * incy;
+
+            while (rem >= 2)
+            {
+                sum1 += x[ix1] * y[iy1];
+                sum2 += x[ix2] * y[iy2];
+                ix1 += incx2;
+                iy1 += incy2;
+                ix2 += incx2;
+                iy2 += incy2;
+                rem -= 2;
+            }
+
+            if (rem == 1)
+            {
+                sum1 += x[ix1] * y[iy1];
+            }
+
+            return sum1 + sum2;
         }
     }
 }
