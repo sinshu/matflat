54b3111015d79b11e4dbbddaacb0d4c16f7b9f06
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-03-16 16:21:44 +0900

    Optimize cholesky.

--- MatFlat/Factorization.Cholesky.cs
+++ MatFlat/Factorization.Cholesky.cs
@@ -9,39 +9,70 @@ namespace MatFlat
         public static unsafe void CholeskyDouble(int n, double* a, int lda)
         {
             var colj = a;
-
             for (var j = 0; j < n; j++)
             {
                 var s = 0.0;
+
+                var colk = a;
                 for (var k = 0; k < j; k++)
                 {
-                    var t = a[k + j * lda];
-                    for (var i = 0; i < k; i++)
-                    {
-                        t -= a[j + i * lda] * a[k + i * lda];
-                    }
-                    t = t / a[k + k * lda];
+                    var t = colj[k] - Dot(k, a + j, a + k, lda);
+                    t = t / colk[k];
 
-                    a[j + k * lda] = t;
+                    colk[j] = t;
                     s += t * t;
+
+                    colk += lda;
                 }
 
-                s = a[j + j * lda] - s;
+                s = colj[j] - s;
 
                 if (s > 0)
                 {
-                    a[j + j * lda] = Math.Sqrt(s);
+                    colj[j] = Math.Sqrt(s);
                 }
                 else
                 {
                     throw new Exception("OMG");
                 }
+
+                colj += lda;
             }
 
             for (var j = 1; j < n; j++)
             {
                 new Span<double>(a + j * lda, j).Clear();
             }
+        }
+
+        private static unsafe T Dot<T>(int n, T* x, T* y, int inc) where T : unmanaged, INumberBase<T>
+        {
+            T sum;
+            switch (n & 1)
+            {
+                case 0:
+                    sum = T.Zero;
+                    break;
+                case 1:
+                    sum = x[0] * y[0];
+                    x += inc;
+                    y += inc;
+                    n--;
+                    break;
+                default:
+                    throw new Exception();
+            }
+
+            var inc2 = 2 * inc;
+            while (n > 0)
+            {
+                sum += x[0] * y[0] + x[inc] * y[inc];
+                x += inc2;
+                y += inc2;
+                n -= 2;
+            }
+
+            return sum;
         }
     }
 }
