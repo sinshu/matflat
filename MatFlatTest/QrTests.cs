7c2385a46b525be39cb340b872d1249f1b244789
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-03-17 15:18:39 +0900

    Update qr test.

--- MatFlatTest/QrTests.cs
+++ MatFlatTest/QrTests.cs
@@ -9,7 +9,20 @@ namespace MatFlatTest
 {
     public class QrTests
     {
+        [TestCase(1, 1, 1)]
+        [TestCase(1, 1, 3)]
+        [TestCase(2, 2, 2)]
+        [TestCase(2, 2, 3)]
         [TestCase(3, 3, 3)]
+        [TestCase(3, 3, 5)]
+        [TestCase(3, 1, 3)]
+        [TestCase(3, 1, 5)]
+        [TestCase(4, 3, 4)]
+        [TestCase(4, 3, 5)]
+        [TestCase(16, 8, 16)]
+        [TestCase(16, 8, 32)]
+        [TestCase(23, 11, 23)]
+        [TestCase(23, 11, 31)]
         public unsafe void QrDouble_General(int m, int n, int lda)
         {
             var a = Matrix.RandomDouble(42, m, n, lda);
@@ -30,11 +43,23 @@ namespace MatFlatTest
                 Factorization.QrDouble(m, n, pa, lda, prdiag);
             }
 
-            Matrix.Print(m, n, expectedA, lda);
-            Console.WriteLine();
-
-            Matrix.Print(m, n, actualA, lda);
-            Console.WriteLine();
+            for (var i = 0; i < a.Length; i++)
+            {
+                var row = i % lda;
+                var col = i / lda;
+                if (col > row)
+                {
+                    Assert.That(actualA[i], Is.EqualTo(expectedA[i]).Within(1.0E-12));
+                }
+                else if (row == col)
+                {
+                    Assert.That(Math.Abs(actualRdiag[col]), Is.EqualTo(Math.Abs(expectedA[i])).Within(1.0E-12));
+                }
+                else if (row >= lda)
+                {
+                    Assert.That(actualA[i], Is.EqualTo(expectedA[i]).Within(1.0E-12));
+                }
+            }
         }
     }
 }
