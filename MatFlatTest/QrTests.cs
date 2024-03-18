c2abe77205581c0c98f5e9979adf791b802afaee
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-03-18 13:27:55 +0900

    Complex QR fully works!!!

--- MatFlatTest/QrTests.cs
+++ MatFlatTest/QrTests.cs
@@ -122,15 +122,23 @@ namespace MatFlatTest
             }
         }
 
+        [TestCase(1, 1, 1, 1, 1)]
+        [TestCase(1, 1, 3, 2, 4)]
         [TestCase(2, 2, 2, 2, 2)]
+        [TestCase(2, 2, 3, 4, 5)]
         [TestCase(3, 3, 3, 3, 3)]
+        [TestCase(3, 3, 5, 4, 6)]
+        [TestCase(3, 1, 3, 3, 1)]
+        [TestCase(3, 1, 5, 4, 6)]
+        [TestCase(4, 3, 4, 4, 3)]
+        [TestCase(4, 3, 5, 7, 6)]
+        [TestCase(16, 8, 16, 16, 8)]
+        [TestCase(16, 8, 32, 24, 20)]
+        [TestCase(23, 11, 23, 23, 11)]
+        [TestCase(23, 11, 31, 29, 37)]
         public unsafe void QrComplex(int m, int n, int lda, int ldq, int ldr)
         {
             var original = Matrix.RandomComplex(42, m, n, lda);
-            //var original = Matrix.RandomDouble(42, m, n, lda).Select(x => (Complex)x).ToArray();
-
-            var openBlas = original.ToArray();
-            var tau = new Complex[n];
 
             var a = original.ToArray();
             var rdiag = new double[n];
@@ -146,8 +154,6 @@ namespace MatFlatTest
             fixed (Complex* pr = r)
             fixed (Complex* preconstructed = reconstructed)
             fixed (Complex* pidentity = identity)
-            fixed (Complex* pob = openBlas)
-            fixed (Complex* ptau = tau)
             {
                 var one = Complex.One;
                 var zero = Complex.Zero;
@@ -172,29 +178,68 @@ namespace MatFlatTest
                     pq, ldq,
                     &zero,
                     pidentity, n);
-
-                Lapack.Zgeqrf(MatrixLayout.ColMajor, m, n, pob, lda, ptau);
             }
 
-            Matrix.Print(m, n, openBlas, lda);
-            Console.WriteLine();
-
-            Matrix.Print(m, n, a, lda);
-            Console.WriteLine();
-
-            foreach (var diag in rdiag)
+            for (var row = 0; row < m; row++)
             {
-                Console.WriteLine(diag);
+                for (var col = 0; col < n; col++)
+                {
+                    var actual = Matrix.Get(m, n, reconstructed, m, row, col);
+                    var expected = Matrix.Get(m, n, original, lda, row, col);
+                    Assert.That(actual.Real, Is.EqualTo(expected.Real).Within(1.0E-12));
+                    Assert.That(actual.Imaginary, Is.EqualTo(expected.Imaginary).Within(1.0E-12));
+                }
             }
 
-            Matrix.Print(n, n, identity, n);
-            Console.WriteLine();
+            for (var row = 0; row < n; row++)
+            {
+                for (var col = 0; col < n; col++)
+                {
+                    var value = identity[col * n + row];
+                    if (row == col)
+                    {
+                        Assert.That(value.Real, Is.EqualTo(1.0).Within(1.0E-12));
+                    }
+                    else
+                    {
+                        Assert.That(value.Real, Is.EqualTo(0.0).Within(1.0E-12));
+                    }
+                    Assert.That(value.Imaginary, Is.EqualTo(0.0).Within(1.0E-12));
+                }
+            }
 
-            Matrix.Print(m, n, reconstructed, m);
-            Console.WriteLine();
+            for (var i = 0; i < a.Length; i++)
+            {
+                var row = i % lda;
+                var col = i / lda;
+                if (row >= m)
+                {
+                    Assert.That(a[i].Real, Is.EqualTo(original[i].Real).Within(0));
+                    Assert.That(a[i].Imaginary, Is.EqualTo(original[i].Imaginary).Within(0));
+                }
+            }
 
-            Matrix.Print(m, n, original, m);
-            Console.WriteLine();
+            for (var i = 0; i < q.Length; i++)
+            {
+                var row = i % ldq;
+                var col = i / ldq;
+                if (row >= m)
+                {
+                    Assert.That(q[i].Real, Is.EqualTo(qCopy[i].Real).Within(0));
+                    Assert.That(q[i].Imaginary, Is.EqualTo(qCopy[i].Imaginary).Within(0));
+                }
+            }
+
+            for (var i = 0; i < r.Length; i++)
+            {
+                var row = i % ldr;
+                var col = i / ldr;
+                if (row >= n)
+                {
+                    Assert.That(r[i].Real, Is.EqualTo(rCopy[i].Real).Within(0));
+                    Assert.That(r[i].Imaginary, Is.EqualTo(rCopy[i].Imaginary).Within(0));
+                }
+            }
         }
     }
 }
