3d625e44bf4d092a5fe14dce573cf15366461eb8
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-03-24 18:00:34 +0900

    Add complex GEVD.

--- MatFlatTest/GevdTests.cs
+++ MatFlatTest/GevdTests.cs
@@ -93,6 +93,75 @@ namespace MatFlatTest
             }
         }
 
+        [TestCase(1, 1, 1)]
+        [TestCase(1, 3, 2)]
+        [TestCase(2, 2, 2)]
+        [TestCase(2, 4, 3)]
+        [TestCase(3, 3, 3)]
+        [TestCase(3, 4, 5)]
+        [TestCase(5, 5, 5)]
+        [TestCase(5, 7, 8)]
+        [TestCase(10, 10, 10)]
+        [TestCase(10, 17, 11)]
+        public unsafe void GevdComplex(int n, int lda, int ldb)
+        {
+            var a = GetDecomposableComplex(42, n, lda);
+            var b = GetDecomposableComplex(57, n, ldb);
+
+            var v = a.ToArray();
+            var l = b.ToArray();
+            var w = new double[n];
+
+            NanLowerPart(n, v, lda);
+            NanLowerPart(n, l, ldb);
+
+            fixed (Complex* pv = v)
+            fixed (Complex* pl = l)
+            fixed (double* pw = w)
+            {
+                Factorization.Gevd(n, pv, lda, pl, ldb, pw);
+            }
+
+            var left = new Complex[n];
+            var right = new Complex[n];
+            fixed (Complex* pa = a)
+            fixed (Complex* pb = b)
+            fixed (Complex* pv = v)
+            fixed (Complex* pl = left)
+            fixed (Complex* pr = right)
+            {
+                var one = Complex.One;
+                var zero = Complex.Zero;
+                for (var j = 0; j < n; j++)
+                {
+                    var cw = (Complex)w[j];
+
+                    OpenBlasSharp.Blas.Zgemv(
+                        Order.ColMajor,
+                        OpenBlasSharp.Transpose.NoTrans,
+                        n, n,
+                        &one,
+                        pa, lda,
+                        pv + lda * j, 1,
+                        &zero,
+                        pl, 1);
+
+                    OpenBlasSharp.Blas.Zgemv(
+                        Order.ColMajor,
+                        OpenBlasSharp.Transpose.NoTrans,
+                        n, n,
+                        &cw,
+                        pb, ldb,
+                        pv + lda * j, 1,
+                        &zero,
+                        pr, 1);
+
+                    Assert.That(left.Select(x => x.Real), Is.EqualTo(right.Select(x => x.Real)).Within(1.0E-11));
+                    Assert.That(left.Select(x => x.Imaginary), Is.EqualTo(right.Select(x => x.Imaginary)).Within(1.0E-11));
+                }
+            }
+        }
+
         private static unsafe double[] GetDecomposableDouble(int seed, int n, int lda)
         {
             var a = Matrix.RandomDouble(seed, n, n, lda);
@@ -124,6 +193,39 @@ namespace MatFlatTest
             return a;
         }
 
+        private static unsafe Complex[] GetDecomposableComplex(int seed, int n, int lda)
+        {
+            var a = Matrix.RandomComplex(seed, n, n, lda);
+
+            var symmetric = new Complex[n * n];
+            fixed (Complex* pa = a)
+            fixed (Complex* ps = symmetric)
+            {
+                var one = Complex.One;
+                var zero = Complex.Zero;
+                OpenBlasSharp.Blas.Zgemm(
+                    Order.ColMajor,
+                    OpenBlasSharp.Transpose.NoTrans,
+                    OpenBlasSharp.Transpose.ConjTrans,
+                    n, n, n,
+                    &one,
+                    pa, lda,
+                    pa, lda,
+                    &zero,
+                    ps, n);
+            }
+
+            for (var row = 0; row < n; row++)
+            {
+                for (var col = 0; col < n; col++)
+                {
+                    a[lda * col + row] = symmetric[n * col + row];
+                }
+            }
+
+            return a;
+        }
+
         private static void NanLowerPart(int n, double[] a, int lda)
         {
             for (var row = 0; row < n; row++)
@@ -133,6 +235,20 @@ namespace MatFlatTest
                     if (row > col)
                     {
                         a[lda * col + row] = double.NaN;
+                    }
+                }
+            }
+        }
+
+        private static void NanLowerPart(int n, Complex[] a, int lda)
+        {
+            for (var row = 0; row < n; row++)
+            {
+                for (var col = 0; col < n; col++)
+                {
+                    if (row > col)
+                    {
+                        a[lda * col + row] = Complex.NaN;
                     }
                 }
             }
