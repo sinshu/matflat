1b21c5bfb2cae12f7d681986d625fac31b500dfd
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-03-26 16:40:50 +0900

    Add mul stub.

--- MatFlat/Blas.Mul.cs
+++ MatFlat/Blas.Mul.cs
@@ -5,6 +5,9 @@ namespace MatFlat
 {
     public static partial class Blas
     {
+        public static unsafe void MulMatMat(Transpose transa, Transpose transb, int m, int n, int k, double* a, int lda, double* b, int ldb, double* c, int ldc)
+        {
 
+        }
     }
 }
