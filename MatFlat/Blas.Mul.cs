9cdfcfd5f0cf8cac3035ecfe3b81d57533ce6756
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-03-26 17:08:39 +0900

    Add matrix multiplication.

--- MatFlat/Blas.Mul.cs
+++ MatFlat/Blas.Mul.cs
@@ -7,7 +7,31 @@ namespace MatFlat
     {
         public static unsafe void MulMatMat(Transpose transa, Transpose transb, int m, int n, int k, double* a, int lda, double* b, int ldb, double* c, int ldc)
         {
-
+            if (transa == Transpose.NoTrans)
+            {
+                if (transb == Transpose.NoTrans)
+                {
+                    var bColj = b;
+                    var cColj = c;
+                    for (var j = 0; j < n; j++)
+                    {
+                        for (var i = 0; i < m; i++)
+                        {
+                            cColj[i] = Internals.Dot(k, a + i, lda, bColj, 1);
+                        }
+                        bColj += ldb;
+                        cColj += ldc;
+                    }
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            else
+            {
+                throw new Exception();
+            }
         }
     }
 }
---
+++ MatFlatTest/BlasTests.MulMatMat.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using NUnit.Framework;
+
+namespace MatFlatTest
+{
+    public class BlasTests_MulMatMat
+    {
+        [TestCase(1, 1, 1, 1, 1, 1)]
+        [TestCase(1, 1, 1, 2, 3, 4)]
+        [TestCase(2, 2, 2, 2, 2, 2)]
+        [TestCase(2, 2, 2, 3, 4, 5)]
+        [TestCase(3, 3, 3, 3, 3, 3)]
+        [TestCase(3, 3, 3, 4, 5, 6)]
+        [TestCase(2, 3, 4, 2, 4, 2)]
+        [TestCase(2, 3, 4, 3, 5, 3)]
+        [TestCase(1, 5, 2, 1, 2, 1)]
+        [TestCase(1, 5, 2, 3, 4, 3)]
+        [TestCase(5, 3, 2, 5, 2, 5)]
+        [TestCase(5, 3, 2, 7, 4, 7)]
+        public unsafe void MulMatMatDouble_NN(int m, int n, int k, int lda, int ldb, int ldc)
+        {
+            var a = Matrix.RandomDouble(42, m, k, lda);
+            var b = Matrix.RandomDouble(57, k, n, ldb);
+            var c = Matrix.RandomDouble(0, m, n, ldc);
+
+            var expected = c.ToArray();
+            fixed (double* pa = a)
+            fixed (double* pb = b)
+            fixed (double* pc = expected)
+            {
+                OpenBlasSharp.Blas.Dgemm(
+                    OpenBlasSharp.Order.ColMajor,
+                    OpenBlasSharp.Transpose.NoTrans, OpenBlasSharp.Transpose.NoTrans,
+                    m, n, k,
+                    1.0,
+                    pa, lda,
+                    pb, ldb,
+                    0.0,
+                    pc, ldc);
+            }
+
+            var actual = c.ToArray();
+            fixed (double* pa = a)
+            fixed (double* pb = b)
+            fixed (double* pc = actual)
+            {
+                MatFlat.Blas.MulMatMat(MatFlat.Transpose.NoTrans, MatFlat.Transpose.NoTrans, m, n, k, pa, lda, pb, ldb, pc, ldc);
+            }
+
+            Assert.That(actual, Is.EqualTo(expected).Within(1.0E-12));
+        }
+    }
+}
