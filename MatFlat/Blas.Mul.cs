f01cebdd4dfaaae35f5989918b3dd9ab318b4381
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-03-26 21:17:08 +0900

    Add single matrix multiplication.

--- MatFlat/Blas.Mul.cs
+++ MatFlat/Blas.Mul.cs
@@ -42,6 +42,182 @@ namespace MatFlat
         /// <param name="ldc">
         /// The leading dimension of the array C.
         /// </param>
+        public static unsafe void MulMatMat(Transpose transa, Transpose transb, int m, int n, int k, float* a, int lda, float* b, int ldb, float* c, int ldc)
+        {
+            if (m <= 0)
+            {
+                throw new ArgumentException("The value must be greater than or equal to one.", nameof(m));
+            }
+
+            if (n <= 0)
+            {
+                throw new ArgumentException("The value must be greater than or equal to one.", nameof(n));
+            }
+
+            if (k <= 0)
+            {
+                throw new ArgumentException("The value must be greater than or equal to one.", nameof(k));
+            }
+
+            if (a == null)
+            {
+                throw new ArgumentNullException(nameof(a));
+            }
+
+            if (transa == Transpose.NoTrans)
+            {
+                if (lda < m)
+                {
+                    throw new ArgumentException("The leading dimension must be greater than or equal to the number of rows.", nameof(lda));
+                }
+            }
+            else if (transa == Transpose.Trans)
+            {
+                if (lda < k)
+                {
+                    throw new ArgumentException("The leading dimension must be greater than or equal to the number of rows.", nameof(lda));
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Invalid enum value.", nameof(transa));
+            }
+
+            if (b == null)
+            {
+                throw new ArgumentNullException(nameof(b));
+            }
+
+            if (transb == Transpose.NoTrans)
+            {
+                if (ldb < k)
+                {
+                    throw new ArgumentException("The leading dimension must be greater than or equal to the number of rows.", nameof(ldb));
+                }
+            }
+            else if (transb == Transpose.Trans)
+            {
+                if (ldb < n)
+                {
+                    throw new ArgumentException("The leading dimension must be greater than or equal to the number of rows.", nameof(ldb));
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Invalid enum value.", nameof(transb));
+            }
+
+            if (c == null)
+            {
+                throw new ArgumentNullException(nameof(c));
+            }
+
+            if (ldc < m)
+            {
+                throw new ArgumentException("The leading dimension must be greater than or equal to the number of rows.", nameof(ldc));
+            }
+
+            if (transa == Transpose.NoTrans)
+            {
+                if (transb == Transpose.NoTrans)
+                {
+                    var bColj = b;
+                    var cColj = c;
+                    for (var j = 0; j < n; j++)
+                    {
+                        for (var i = 0; i < m; i++)
+                        {
+                            cColj[i] = (float)Internals.Dot(k, a + i, lda, bColj, 1);
+                        }
+                        bColj += ldb;
+                        cColj += ldc;
+                    }
+                }
+                else
+                {
+                    var cColj = c;
+                    for (var j = 0; j < n; j++)
+                    {
+                        for (var i = 0; i < m; i++)
+                        {
+                            cColj[i] = (float)Internals.Dot(k, a + i, lda, b + j, ldb);
+                        }
+                        cColj += ldc;
+                    }
+                }
+            }
+            else
+            {
+                if (transb == Transpose.NoTrans)
+                {
+                    var bColj = b;
+                    var cColj = c;
+                    for (var j = 0; j < n; j++)
+                    {
+                        var aColi = a;
+                        for (var i = 0; i < m; i++)
+                        {
+                            cColj[i] = (float)Internals.Dot(k, aColi, bColj);
+                            aColi += lda;
+                        }
+                        bColj += ldb;
+                        cColj += ldc;
+                    }
+                }
+                else
+                {
+                    var cColj = c;
+                    for (var j = 0; j < n; j++)
+                    {
+                        var aColi = a;
+                        for (var i = 0; i < m; i++)
+                        {
+                            cColj[i] = (float)Internals.Dot(k, aColi, 1, b + j, ldb);
+                            aColi += lda;
+                        }
+                        cColj += ldc;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Computes a matrix multiplication <c>C = A * B</c>.
+        /// </summary>
+        /// <param name="transa">
+        /// Specifies whether the matrix A is treated as transposed or not.
+        /// </param>
+        /// <param name="transb">
+        /// Specifies whether the matrix B is treated as transposed or not.
+        /// </param>
+        /// <param name="m">
+        /// The number of rows of the matrix A.
+        /// </param>
+        /// <param name="n">
+        /// The number of columns of the matrix B.
+        /// </param>
+        /// <param name="k">
+        /// The number of columns of the matrix A.
+        /// This is equal to the number of rows of the matrix B.
+        /// </param>
+        /// <param name="a">
+        /// The matrix A.
+        /// </param>
+        /// <param name="lda">
+        /// The leading dimension of the array A.
+        /// </param>
+        /// <param name="b">
+        /// The matrix B.
+        /// </param>
+        /// <param name="ldb">
+        /// The leading dimension of the array B.
+        /// </param>
+        /// <param name="c">
+        /// The matrix C, which is the result of the multiplication.
+        /// </param>
+        /// <param name="ldc">
+        /// The leading dimension of the array C.
+        /// </param>
         public static unsafe void MulMatMat(Transpose transa, Transpose transb, int m, int n, int k, double* a, int lda, double* b, int ldb, double* c, int ldc)
         {
             if (m <= 0)
--- MatFlatTest/BlasTests.MulMatMatSingle.cs
+++ MatFlatTest/BlasTests.MulMatMatSingle.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Linq;
-using System.Numerics;
 using NUnit.Framework;
 
 namespace MatFlatTest
 {
-    public class BlasTests_MulMatMat
+    public class BlasTests_MulMatMatSingle
     {
         [TestCase(1, 1, 1, 1, 1, 1)]
         [TestCase(1, 1, 1, 2, 3, 4)]
@@ -19,37 +18,37 @@ namespace MatFlatTest
         [TestCase(1, 5, 2, 3, 4, 3)]
         [TestCase(5, 3, 2, 5, 2, 5)]
         [TestCase(5, 3, 2, 7, 4, 7)]
-        public unsafe void MulMatMatDouble_NN(int m, int n, int k, int lda, int ldb, int ldc)
+        public unsafe void NN(int m, int n, int k, int lda, int ldb, int ldc)
         {
-            var a = Matrix.RandomDouble(42, m, k, lda);
-            var b = Matrix.RandomDouble(57, k, n, ldb);
-            var c = Matrix.RandomDouble(0, m, n, ldc);
+            var a = Matrix.RandomSingle(42, m, k, lda);
+            var b = Matrix.RandomSingle(57, k, n, ldb);
+            var c = Matrix.RandomSingle(0, m, n, ldc);
 
             var expected = c.ToArray();
-            fixed (double* pa = a)
-            fixed (double* pb = b)
-            fixed (double* pc = expected)
+            fixed (float* pa = a)
+            fixed (float* pb = b)
+            fixed (float* pc = expected)
             {
-                OpenBlasSharp.Blas.Dgemm(
+                OpenBlasSharp.Blas.Sgemm(
                     OpenBlasSharp.Order.ColMajor,
                     OpenBlasSharp.Transpose.NoTrans, OpenBlasSharp.Transpose.NoTrans,
                     m, n, k,
-                    1.0,
+                    1.0F,
                     pa, lda,
                     pb, ldb,
-                    0.0,
+                    0.0F,
                     pc, ldc);
             }
 
             var actual = c.ToArray();
-            fixed (double* pa = a)
-            fixed (double* pb = b)
-            fixed (double* pc = actual)
+            fixed (float* pa = a)
+            fixed (float* pb = b)
+            fixed (float* pc = actual)
             {
                 MatFlat.Blas.MulMatMat(MatFlat.Transpose.NoTrans, MatFlat.Transpose.NoTrans, m, n, k, pa, lda, pb, ldb, pc, ldc);
             }
 
-            Assert.That(actual, Is.EqualTo(expected).Within(1.0E-12));
+            Assert.That(actual, Is.EqualTo(expected).Within(1.0E-6));
         }
 
         [TestCase(1, 1, 1, 1, 1, 1)]
@@ -61,37 +60,37 @@ namespace MatFlatTest
         [TestCase(5, 4, 3, 4, 5, 6)]
         [TestCase(3, 7, 5, 5, 5, 3)]
         [TestCase(3, 7, 5, 8, 9, 7)]
-        public unsafe void MulMatMatDouble_TN(int m, int n, int k, int lda, int ldb, int ldc)
+        public unsafe void TN(int m, int n, int k, int lda, int ldb, int ldc)
         {
-            var a = Matrix.RandomDouble(42, k, m, lda);
-            var b = Matrix.RandomDouble(57, k, n, ldb);
-            var c = Matrix.RandomDouble(0, m, n, ldc);
+            var a = Matrix.RandomSingle(42, k, m, lda);
+            var b = Matrix.RandomSingle(57, k, n, ldb);
+            var c = Matrix.RandomSingle(0, m, n, ldc);
 
             var expected = c.ToArray();
-            fixed (double* pa = a)
-            fixed (double* pb = b)
-            fixed (double* pc = expected)
+            fixed (float* pa = a)
+            fixed (float* pb = b)
+            fixed (float* pc = expected)
             {
-                OpenBlasSharp.Blas.Dgemm(
+                OpenBlasSharp.Blas.Sgemm(
                     OpenBlasSharp.Order.ColMajor,
                     OpenBlasSharp.Transpose.Trans, OpenBlasSharp.Transpose.NoTrans,
                     m, n, k,
-                    1.0,
+                    1.0F,
                     pa, lda,
                     pb, ldb,
-                    0.0,
+                    0.0F,
                     pc, ldc);
             }
 
             var actual = c.ToArray();
-            fixed (double* pa = a)
-            fixed (double* pb = b)
-            fixed (double* pc = actual)
+            fixed (float* pa = a)
+            fixed (float* pb = b)
+            fixed (float* pc = actual)
             {
                 MatFlat.Blas.MulMatMat(MatFlat.Transpose.Trans, MatFlat.Transpose.NoTrans, m, n, k, pa, lda, pb, ldb, pc, ldc);
             }
 
-            Assert.That(actual, Is.EqualTo(expected).Within(1.0E-12));
+            Assert.That(actual, Is.EqualTo(expected).Within(1.0E-6));
         }
 
         [TestCase(1, 1, 1, 1, 1, 1)]
@@ -103,37 +102,37 @@ namespace MatFlatTest
         [TestCase(5, 4, 3, 6, 6, 7)]
         [TestCase(3, 7, 5, 3, 7, 3)]
         [TestCase(3, 7, 5, 5, 9, 6)]
-        public unsafe void MulMatMatDouble_NT(int m, int n, int k, int lda, int ldb, int ldc)
+        public unsafe void NT(int m, int n, int k, int lda, int ldb, int ldc)
         {
-            var a = Matrix.RandomDouble(42, m, k, lda);
-            var b = Matrix.RandomDouble(57, n, k, ldb);
-            var c = Matrix.RandomDouble(0, m, n, ldc);
+            var a = Matrix.RandomSingle(42, m, k, lda);
+            var b = Matrix.RandomSingle(57, n, k, ldb);
+            var c = Matrix.RandomSingle(0, m, n, ldc);
 
             var expected = c.ToArray();
-            fixed (double* pa = a)
-            fixed (double* pb = b)
-            fixed (double* pc = expected)
+            fixed (float* pa = a)
+            fixed (float* pb = b)
+            fixed (float* pc = expected)
             {
-                OpenBlasSharp.Blas.Dgemm(
+                OpenBlasSharp.Blas.Sgemm(
                     OpenBlasSharp.Order.ColMajor,
                     OpenBlasSharp.Transpose.NoTrans, OpenBlasSharp.Transpose.Trans,
                     m, n, k,
-                    1.0,
+                    1.0F,
                     pa, lda,
                     pb, ldb,
-                    0.0,
+                    0.0F,
                     pc, ldc);
             }
 
             var actual = c.ToArray();
-            fixed (double* pa = a)
-            fixed (double* pb = b)
-            fixed (double* pc = actual)
+            fixed (float* pa = a)
+            fixed (float* pb = b)
+            fixed (float* pc = actual)
             {
                 MatFlat.Blas.MulMatMat(MatFlat.Transpose.NoTrans, MatFlat.Transpose.Trans, m, n, k, pa, lda, pb, ldb, pc, ldc);
             }
 
-            Assert.That(actual, Is.EqualTo(expected).Within(1.0E-12));
+            Assert.That(actual, Is.EqualTo(expected).Within(1.0E-6));
         }
 
         [TestCase(1, 1, 1, 1, 1, 1)]
@@ -145,37 +144,37 @@ namespace MatFlatTest
         [TestCase(5, 4, 3, 7, 5, 6)]
         [TestCase(3, 7, 5, 5, 7, 3)]
         [TestCase(3, 7, 5, 6, 8, 4)]
-        public unsafe void MulMatMatDouble_TT(int m, int n, int k, int lda, int ldb, int ldc)
+        public unsafe void TT(int m, int n, int k, int lda, int ldb, int ldc)
         {
-            var a = Matrix.RandomDouble(42, k, m, lda);
-            var b = Matrix.RandomDouble(57, n, k, ldb);
-            var c = Matrix.RandomDouble(0, m, n, ldc);
+            var a = Matrix.RandomSingle(42, k, m, lda);
+            var b = Matrix.RandomSingle(57, n, k, ldb);
+            var c = Matrix.RandomSingle(0, m, n, ldc);
 
             var expected = c.ToArray();
-            fixed (double* pa = a)
-            fixed (double* pb = b)
-            fixed (double* pc = expected)
+            fixed (float* pa = a)
+            fixed (float* pb = b)
+            fixed (float* pc = expected)
             {
-                OpenBlasSharp.Blas.Dgemm(
+                OpenBlasSharp.Blas.Sgemm(
                     OpenBlasSharp.Order.ColMajor,
                     OpenBlasSharp.Transpose.Trans, OpenBlasSharp.Transpose.Trans,
                     m, n, k,
-                    1.0,
+                    1.0F,
                     pa, lda,
                     pb, ldb,
-                    0.0,
+                    0.0F,
                     pc, ldc);
             }
 
             var actual = c.ToArray();
-            fixed (double* pa = a)
-            fixed (double* pb = b)
-            fixed (double* pc = actual)
+            fixed (float* pa = a)
+            fixed (float* pb = b)
+            fixed (float* pc = actual)
             {
                 MatFlat.Blas.MulMatMat(MatFlat.Transpose.Trans, MatFlat.Transpose.Trans, m, n, k, pa, lda, pb, ldb, pc, ldc);
             }
 
-            Assert.That(actual, Is.EqualTo(expected).Within(1.0E-12));
+            Assert.That(actual, Is.EqualTo(expected).Within(1.0E-6));
         }
     }
 }
