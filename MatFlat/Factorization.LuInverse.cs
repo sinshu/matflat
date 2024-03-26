21f65a607187fc9c375e572b56e63ec5f85289b4
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-03-26 11:49:31 +0900

    Add single inverse matrix.

--- MatFlat/Factorization.LuInverse.cs
+++ MatFlat/Factorization.LuInverse.cs
@@ -24,6 +24,102 @@ namespace MatFlat
         /// <exception cref="MatrixFactorizationException">
         /// The matrix is not invertible.
         /// </exception>
+        public static unsafe void LuInverse(int n, float* a, int lda, int* piv)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentException("The order of the matrix must be greater than or equal to one.", nameof(n));
+            }
+
+            if (a == null)
+            {
+                throw new ArgumentNullException(nameof(a));
+            }
+
+            if (lda < n)
+            {
+                throw new ArgumentException("The leading dimension must be greater than or equal to the order of the matrix.", nameof(lda));
+            }
+
+            if (piv == null)
+            {
+                throw new ArgumentNullException(nameof(piv));
+            }
+
+            for (var j = 0; j < n; j++)
+            {
+                var aColj = a + lda * j;
+                if (aColj[j] == 0.0)
+                {
+                    throw new MatrixFactorizationException("The matrix is not invertible.");
+                }
+            }
+
+            var work = ArrayPool<float>.Shared.Rent(n * n + n);
+            try
+            {
+                fixed (float* pwork = work)
+                {
+                    var diag = pwork + n * n;
+                    for (var j = 0; j < n; j++)
+                    {
+                        var aColj = a + lda * j;
+                        var copySize = sizeof(float) * n;
+                        Buffer.MemoryCopy(aColj, pwork + n * j, copySize, copySize);
+                        diag[j] = aColj[j];
+                        new Span<float>(aColj, n).Clear();
+                    }
+
+                    for (var j = 0; j < n; j++)
+                    {
+                        a[lda * piv[j] + j] = 1.0F;
+                    }
+
+                    for (var j = 0; j < n; j++)
+                    {
+                        pwork[n * j + j] = 1.0F;
+                    }
+                    for (var j = 0; j < n; j++)
+                    {
+                        var aColj = a + lda * j;
+                        Blas.SolveTriangular(Uplo.Lower, Transpose.NoTrans, n, pwork, n, aColj, 1);
+                    }
+
+                    for (var j = 0; j < n; j++)
+                    {
+                        pwork[n * j + j] = diag[j];
+                    }
+                    for (var j = 0; j < n; j++)
+                    {
+                        var aColj = a + lda * j;
+                        Blas.SolveTriangular(Uplo.Upper, Transpose.NoTrans, n, pwork, n, aColj, 1);
+                    }
+                }
+            }
+            finally
+            {
+                ArrayPool<float>.Shared.Return(work);
+            }
+        }
+
+        /// <summary>
+        /// Computes the inverse matrix from the result of an LU decomposition.
+        /// </summary>
+        /// <param name="n">
+        /// The order of the matrix.
+        /// </param>
+        /// <param name="a">
+        /// The result of the LU decomposition.
+        /// </param>
+        /// <param name="lda">
+        /// The leading dimension.
+        /// </param>
+        /// <param name="piv">
+        /// The pivot indices obtained from the LU decomposition.
+        /// </param>
+        /// <exception cref="MatrixFactorizationException">
+        /// The matrix is not invertible.
+        /// </exception>
         public static unsafe void LuInverse(int n, double* a, int lda, int* piv)
         {
             if (n <= 0)
@@ -94,8 +190,6 @@ namespace MatFlat
                         var aColj = a + lda * j;
                         Blas.SolveTriangular(Uplo.Upper, Transpose.NoTrans, n, pwork, n, aColj, 1);
                     }
-
-
                 }
             }
             finally
--- MatFlatTest/LuInverseTests.cs
+++ MatFlatTest/LuInverseTests.cs
@@ -19,6 +19,69 @@ namespace MatFlatTest
         [TestCase(5, 8)]
         [TestCase(10, 10)]
         [TestCase(10, 17)]
+        public unsafe void LuInverseSingle(int n, int lda)
+        {
+            var original = Matrix.RandomSingle(42, n, n, lda);
+
+            var a = original.ToArray();
+            var piv = new int[n];
+            var identity = new float[n * n];
+            fixed (float* pa = a)
+            fixed (int* ppiv = piv)
+            fixed (float* poriginal = original)
+            fixed (float* pidentity = identity)
+            {
+                Factorization.Lu(n, n, pa, lda, ppiv);
+                Factorization.LuInverse(n, pa, lda, ppiv);
+                OpenBlasSharp.Blas.Sgemm(
+                    Order.ColMajor,
+                    OpenBlasSharp.Transpose.NoTrans,
+                    OpenBlasSharp.Transpose.NoTrans,
+                    n, n, n,
+                    1.0F,
+                    poriginal, lda,
+                    pa, lda,
+                    0.0F,
+                    pidentity, n);
+            }
+
+            for (var row = 0; row < n; row++)
+            {
+                for (var col = 0; col < n; col++)
+                {
+                    var value = identity[col * n + row];
+                    if (row == col)
+                    {
+                        Assert.That(value, Is.EqualTo(1.0).Within(1.0E-5));
+                    }
+                    else
+                    {
+                        Assert.That(value, Is.EqualTo(0.0).Within(1.0E-5));
+                    }
+                }
+            }
+
+            for (var i = 0; i < a.Length; i++)
+            {
+                var row = i % lda;
+                var col = i / lda;
+                if (row >= n)
+                {
+                    Assert.That(a[i], Is.EqualTo(original[i]).Within(0));
+                }
+            }
+        }
+
+        [TestCase(1, 1)]
+        [TestCase(1, 3)]
+        [TestCase(2, 2)]
+        [TestCase(2, 3)]
+        [TestCase(3, 3)]
+        [TestCase(3, 5)]
+        [TestCase(5, 5)]
+        [TestCase(5, 8)]
+        [TestCase(10, 10)]
+        [TestCase(10, 17)]
         public unsafe void LuInverseDouble(int n, int lda)
         {
             var original = Matrix.RandomDouble(42, n, n, lda);
