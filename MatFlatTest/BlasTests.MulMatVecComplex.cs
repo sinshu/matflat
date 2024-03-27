749540c3d59c21df32d6c1a60e264ba37bcaef3a
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-03-27 11:27:11 +0900

    Add complex mat-vec multiplication.

--- MatFlatTest/BlasTests.MulMatVecComplex.cs
+++ MatFlatTest/BlasTests.MulMatVecComplex.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Linq;
+using System.Numerics;
 using NUnit.Framework;
 
 namespace MatFlatTest
 {
-    public class BlasTests_MulMatVecSingle
+    public class BlasTests_MulMatVecComplex
     {
         [TestCase(1, 1, 1, 1, 1)]
         [TestCase(1, 1, 3, 2, 4)]
@@ -22,35 +23,39 @@ namespace MatFlatTest
         [TestCase(8, 3, 9, 2, 2)]
         public unsafe void NoTrans(int m, int n, int lda, int incx, int incy)
         {
-            var a = Matrix.RandomSingle(42, m, n, lda);
-            var x = Vector.RandomSingle(57, n, incx);
-            var y = Vector.RandomSingle(0, m, incy);
+            var a = Matrix.RandomComplex(42, m, n, lda);
+            var x = Vector.RandomComplex(57, n, incx);
+            var y = Vector.RandomComplex(0, m, incy);
 
             var expected = y.ToArray();
-            fixed (float* pa = a)
-            fixed (float* px = x)
-            fixed (float* py = expected)
+            fixed (Complex* pa = a)
+            fixed (Complex* px = x)
+            fixed (Complex* py = expected)
             {
-                OpenBlasSharp.Blas.Sgemv(
+                var one = Complex.One;
+                var zero = Complex.Zero;
+
+                OpenBlasSharp.Blas.Zgemv(
                     OpenBlasSharp.Order.ColMajor,
                     OpenBlasSharp.Transpose.NoTrans,
                     m, n,
-                    1.0F,
+                    &one,
                     pa, lda,
                     px, incx,
-                    0.0F,
+                    &zero,
                     py, incy);
             }
 
             var actual = y.ToArray();
-            fixed (float* pa = a)
-            fixed (float* px = x)
-            fixed (float* py = actual)
+            fixed (Complex* pa = a)
+            fixed (Complex* px = x)
+            fixed (Complex* py = actual)
             {
                 MatFlat.Blas.MulMatVec(MatFlat.Transpose.NoTrans, m, n, pa, lda, px, incx, py, incy);
             }
 
-            Assert.That(actual, Is.EqualTo(expected).Within(1.0E-5));
+            Assert.That(actual.Select(x => x.Real), Is.EqualTo(expected.Select(x => x.Real)).Within(1.0E-12));
+            Assert.That(actual.Select(x => x.Imaginary), Is.EqualTo(expected.Select(x => x.Imaginary)).Within(1.0E-12));
         }
 
         [TestCase(1, 1, 1, 1, 1)]
@@ -63,41 +68,45 @@ namespace MatFlatTest
         [TestCase(1, 5, 2, 2, 2)]
         [TestCase(4, 1, 4, 1, 1)]
         [TestCase(4, 1, 5, 3, 4)]
-        [TestCase(4, 9, 4, 1, 1)]
-        [TestCase(4, 9, 5, 3, 2)]
+        [TestCase(5, 9, 5, 1, 1)]
+        [TestCase(5, 9, 6, 3, 2)]
         [TestCase(8, 3, 8, 1, 1)]
         [TestCase(8, 3, 9, 2, 2)]
         public unsafe void Trans(int m, int n, int lda, int incx, int incy)
         {
-            var a = Matrix.RandomSingle(42, m, n, lda);
-            var x = Vector.RandomSingle(57, n, incx);
-            var y = Vector.RandomSingle(0, m, incy);
+            var a = Matrix.RandomComplex(42, m, n, lda);
+            var x = Vector.RandomComplex(57, n, incx);
+            var y = Vector.RandomComplex(0, m, incy);
 
             var expected = y.ToArray();
-            fixed (float* pa = a)
-            fixed (float* px = x)
-            fixed (float* py = expected)
+            fixed (Complex* pa = a)
+            fixed (Complex* px = x)
+            fixed (Complex* py = expected)
             {
-                OpenBlasSharp.Blas.Sgemv(
+                var one = Complex.One;
+                var zero = Complex.Zero;
+
+                OpenBlasSharp.Blas.Zgemv(
                     OpenBlasSharp.Order.ColMajor,
                     OpenBlasSharp.Transpose.Trans,
                     m, n,
-                    1.0F,
+                    &one,
                     pa, lda,
                     px, incx,
-                    0.0F,
+                    &zero,
                     py, incy);
             }
 
             var actual = y.ToArray();
-            fixed (float* pa = a)
-            fixed (float* px = x)
-            fixed (float* py = actual)
+            fixed (Complex* pa = a)
+            fixed (Complex* px = x)
+            fixed (Complex* py = actual)
             {
                 MatFlat.Blas.MulMatVec(MatFlat.Transpose.Trans, m, n, pa, lda, px, incx, py, incy);
             }
 
-            Assert.That(actual, Is.EqualTo(expected).Within(1.0E-5));
+            Assert.That(actual.Select(x => x.Real), Is.EqualTo(expected.Select(x => x.Real)).Within(1.0E-12));
+            Assert.That(actual.Select(x => x.Imaginary), Is.EqualTo(expected.Select(x => x.Imaginary)).Within(1.0E-12));
         }
     }
 }
