02fb35bc4e6ddefcef1f27dbdf2b2383e1464041
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-03-27 10:26:40 +0900

    Add mat-vec multiplication.

---
+++ MatFlat/Blas.MulMatVec.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Numerics;
+
+namespace MatFlat
+{
+    public static partial class Blas
+    {
+        /// <summary>
+        /// Computes a matrix-and-vector multiplication <c>y = A * x</c>.
+        /// </summary>
+        /// <param name="trans">
+        /// Specifies whether the matrix A is treated as transposed or not.
+        /// </param>
+        /// <param name="m">
+        /// The number of rows of the matrix A.
+        /// </param>
+        /// <param name="n">
+        /// The number of columns of the matrix A.
+        /// </param>
+        /// <param name="a">
+        /// The matrix A.
+        /// </param>
+        /// <param name="lda">
+        /// The leading dimension of the array A.
+        /// </param>
+        /// <param name="x">
+        /// The vector x.
+        /// </param>
+        /// <param name="incx">
+        /// The stride for the elements of the array x.
+        /// </param>
+        /// <param name="y">
+        /// The vector y, which is the result of the multiplication.
+        /// </param>
+        /// <param name="incy">
+        /// The stride for the elements of the array y.
+        /// </param>
+        public static unsafe void MulMatVec(Transpose trans, int m, int n, float* a, int lda, float* x, int incx, float* y, int incy)
+        {
+            if (m <= 0)
+            {
+                throw new ArgumentException("The number of rows must be greater than or equal to one.", nameof(m));
+            }
+
+            if (n <= 0)
+            {
+                throw new ArgumentException("The number of columns must be greater than or equal to one.", nameof(n));
+            }
+
+            if (a == null)
+            {
+                throw new ArgumentNullException(nameof(a));
+            }
+
+            if (lda < m)
+            {
+                throw new ArgumentException("The leading dimension must be greater than or equal to the number of rows.", nameof(lda));
+            }
+
+            if (x == null)
+            {
+                throw new ArgumentNullException(nameof(x));
+            }
+
+            if (incx <= 0)
+            {
+                throw new ArgumentException("The value must be greater than or equal to one.", nameof(incx));
+            }
+
+            if (y == null)
+            {
+                throw new ArgumentNullException(nameof(y));
+            }
+
+            if (incy <= 0)
+            {
+                throw new ArgumentException("The value must be greater than or equal to one.", nameof(incy));
+            }
+
+            if (trans == Transpose.NoTrans)
+            {
+                for (var i = 0; i < m; i++)
+                {
+                    *y = (float)Internals.Dot(n, a + i, lda, x, incx);
+                    y += incy;
+                }
+            }
+            else if (trans == Transpose.Trans)
+            {
+                for (var i = 0; i < n; i++)
+                {
+                    *y = (float)Internals.Dot(m, a, 1, x, incx);
+                    a += lda;
+                    y += incy;
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Invalid enum value.", nameof(trans));
+            }
+        }
+
+        /// <summary>
+        /// Computes a matrix-and-vector multiplication <c>y = A * x</c>.
+        /// </summary>
+        /// <param name="trans">
+        /// Specifies whether the matrix A is treated as transposed or not.
+        /// </param>
+        /// <param name="m">
+        /// The number of rows of the matrix A.
+        /// </param>
+        /// <param name="n">
+        /// The number of columns of the matrix A.
+        /// </param>
+        /// <param name="a">
+        /// The matrix A.
+        /// </param>
+        /// <param name="lda">
+        /// The leading dimension of the array A.
+        /// </param>
+        /// <param name="x">
+        /// The vector x.
+        /// </param>
+        /// <param name="incx">
+        /// The stride for the elements of the array x.
+        /// </param>
+        /// <param name="y">
+        /// The vector y, which is the result of the multiplication.
+        /// </param>
+        /// <param name="incy">
+        /// The stride for the elements of the array y.
+        /// </param>
+        public static unsafe void MulMatVec(Transpose trans, int m, int n, double* a, int lda, double* x, int incx, double* y, int incy)
+        {
+            if (m <= 0)
+            {
+                throw new ArgumentException("The number of rows must be greater than or equal to one.", nameof(m));
+            }
+
+            if (n <= 0)
+            {
+                throw new ArgumentException("The number of columns must be greater than or equal to one.", nameof(n));
+            }
+
+            if (a == null)
+            {
+                throw new ArgumentNullException(nameof(a));
+            }
+
+            if (lda < m)
+            {
+                throw new ArgumentException("The leading dimension must be greater than or equal to the number of rows.", nameof(lda));
+            }
+
+            if (x == null)
+            {
+                throw new ArgumentNullException(nameof(x));
+            }
+
+            if (incx <= 0)
+            {
+                throw new ArgumentException("The value must be greater than or equal to one.", nameof(incx));
+            }
+
+            if (y == null)
+            {
+                throw new ArgumentNullException(nameof(y));
+            }
+
+            if (incy <= 0)
+            {
+                throw new ArgumentException("The value must be greater than or equal to one.", nameof(incy));
+            }
+
+            if (trans == Transpose.NoTrans)
+            {
+                for (var i = 0; i < m; i++)
+                {
+                    *y = Internals.Dot(n, a + i, lda, x, incx);
+                    y += incy;
+                }
+            }
+            else if (trans == Transpose.Trans)
+            {
+                for (var i = 0; i < n; i++)
+                {
+                    *y = Internals.Dot(m, a, 1, x, incx);
+                    a += lda;
+                    y += incy;
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Invalid enum value.", nameof(trans));
+            }
+        }
+    }
+}
---
+++ MatFlatTest/BlasTests.MulMatVecSingle.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace MatFlatTest
+{
+    public class BlasTests_MulMatVecSingle
+    {
+        [TestCase(1, 1, 1, 1, 1)]
+        [TestCase(1, 1, 3, 2, 4)]
+        [TestCase(2, 2, 2, 1, 1)]
+        [TestCase(2, 2, 4, 3, 3)]
+        [TestCase(3, 3, 3, 1, 1)]
+        [TestCase(3, 3, 5, 6, 7)]
+        [TestCase(1, 5, 1, 1, 1)]
+        [TestCase(1, 5, 3, 2, 4)]
+        [TestCase(4, 1, 4, 1, 1)]
+        [TestCase(4, 1, 6, 5, 7)]
+        [TestCase(4, 9, 4, 1, 1)]
+        [TestCase(4, 9, 5, 3, 2)]
+        [TestCase(8, 3, 8, 1, 1)]
+        [TestCase(8, 3, 9, 2, 2)]
+        public unsafe void NoTrans(int m, int n, int lda, int incx, int incy)
+        {
+            var a = Matrix.RandomSingle(42, m, n, lda);
+            var x = Vector.RandomSingle(57, n, incx);
+            var y = Vector.RandomSingle(0, m, incy);
+
+            var expected = y.ToArray();
+            fixed (float* pa = a)
+            fixed (float* px = x)
+            fixed (float* py = expected)
+            {
+                OpenBlasSharp.Blas.Sgemv(
+                    OpenBlasSharp.Order.ColMajor,
+                    OpenBlasSharp.Transpose.NoTrans,
+                    m, n,
+                    1.0F,
+                    pa, lda,
+                    px, incx,
+                    0.0F,
+                    py, incy);
+            }
+
+            var actual = y.ToArray();
+            fixed (float* pa = a)
+            fixed (float* px = x)
+            fixed (float* py = actual)
+            {
+                MatFlat.Blas.MulMatVec(MatFlat.Transpose.NoTrans, m, n, pa, lda, px, incx, py, incy);
+            }
+
+            Assert.That(actual, Is.EqualTo(expected).Within(1.0E-6));
+        }
+
+        [TestCase(1, 1, 1, 1, 1)]
+        [TestCase(1, 1, 3, 2, 4)]
+        [TestCase(2, 2, 2, 1, 1)]
+        [TestCase(2, 2, 3, 2, 3)]
+        [TestCase(3, 3, 3, 1, 1)]
+        [TestCase(3, 3, 4, 3, 2)]
+        [TestCase(1, 5, 1, 1, 1)]
+        [TestCase(1, 5, 2, 2, 2)]
+        [TestCase(4, 1, 4, 1, 1)]
+        [TestCase(4, 1, 5, 3, 4)]
+        [TestCase(4, 9, 4, 1, 1)]
+        [TestCase(4, 9, 5, 3, 2)]
+        [TestCase(8, 3, 8, 1, 1)]
+        [TestCase(8, 3, 9, 2, 2)]
+        public unsafe void Trans(int m, int n, int lda, int incx, int incy)
+        {
+            var a = Matrix.RandomSingle(42, m, n, lda);
+            var x = Vector.RandomSingle(57, n, incx);
+            var y = Vector.RandomSingle(0, m, incy);
+
+            var expected = y.ToArray();
+            fixed (float* pa = a)
+            fixed (float* px = x)
+            fixed (float* py = expected)
+            {
+                OpenBlasSharp.Blas.Sgemv(
+                    OpenBlasSharp.Order.ColMajor,
+                    OpenBlasSharp.Transpose.Trans,
+                    m, n,
+                    1.0F,
+                    pa, lda,
+                    px, incx,
+                    0.0F,
+                    py, incy);
+            }
+
+            var actual = y.ToArray();
+            fixed (float* pa = a)
+            fixed (float* px = x)
+            fixed (float* py = actual)
+            {
+                MatFlat.Blas.MulMatVec(MatFlat.Transpose.Trans, m, n, pa, lda, px, incx, py, incy);
+            }
+
+            Assert.That(actual, Is.EqualTo(expected).Within(1.0E-6));
+        }
+    }
+}
